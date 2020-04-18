680a8222c290b1993e5978e211038b060b223737
Bohdan Holoborodko <holoborodko.bohdan@gmail.com>
2020-04-18 18:56:35 +0300

    django-filter added to EmployeeView

--- scraping/views.py
+++ scraping/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.generics import CreateAPIView
+from .models import Employee
+from rest_framework.generics import ListCreateAPIView
+from django_filters import rest_framework as filters
 
 from scraping.serializers import EmployeeSerializer
 
 
-class CreateEmployeeView(CreateAPIView):
+class EmployeeView(ListCreateAPIView):
+    queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('position',)
