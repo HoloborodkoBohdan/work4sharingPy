5cb9619feaacddbec292e62a06eb102ca856061e
Bohdan Holoborodko <holoborodko.bohdan@gmail.com>
2020-05-03 03:59:37 +0300

    admin edit view for some models

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from scraping.models import Employee, Job, Request, Skill
 
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    model = Skill
+    list_display = ['name', 'isset', 'link']
+    search_fields = ['name']
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    model = Job
+    list_display = ['title', 'site', 'company_name', 'location', 'industry', 'date_created']
+    search_fields = ['title', 'company_name']
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    model = Employee
+    list_display = ['position', 'status', 'conformity']
+    raw_id_fields = ['related_request', 'related_vacancy']
+    filter_horizontal = ['skills',]
+
+
 admin.site.register(Request)
-admin.site.register(Employee)
-admin.site.register(Job)
-admin.site.register(Skill)
